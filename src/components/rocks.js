0bef4d85ec07fa6a0c4fdeeb8e461839dac7ab27
[zanzers] <[nowellsaavedra999@gmail.com]>
2024-12-29 21:10:26 +0800

    models

---
+++ src/components/rocks.js
@@ -0,0 +1,31 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+
+class Rocks{
+    constructor(scene){
+
+        this.loader = new GLTFLoader();
+
+        this.loader.load(
+            '../models/Rocks.glb',
+            (gltf) => {
+                const model = gltf.scene;
+
+                model.position.set(0,0,3);
+                model.scale.set(1,1,1);
+
+                scene.add(gltf.scene);
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+            },
+            (error) => {
+                console.error("error occured", error);
+            }
+        )
+    }
+}
+
+
+export default Rocks;
